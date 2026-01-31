eddcc7ac4451b07c9365723bea6c2e425b4b3d10
Cristian Angelo Manica Georgiev <cmanica-@car8s2.42barcelona.com>
2026-01-31 12:40:36 +0100

    moved binaries for cgi

--- cgi-bin/broken.py
+++ cgi-bin/broken.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-# Crash immediately without printing headers
 sys.stderr.write("I am crashing now!\n")
 exit(1)
--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -2,32 +2,20 @@
 import os
 import sys
 
-# --- 1. SEND HEADERS ---
-# The server expects \r\n\r\n to separate headers from body
 print("Content-Type: text/html\r\n\r\n", end="")
-
-# --- 2. HTML OUTPUT ---
-print("<html><head><title>CGI Test</title></head><body>")
 print("<h1 style='color:green;'>CGI works!</h1>")
 
-# --- 3. DUMP ENVIRONMENT VARIABLES ---
-# This verifies PATH_INFO, QUERY_STRING, REMOTE_ADDR, etc.
-print("<h2>Environment Variables</h2>")
-print("<table border='1' cellpadding='5'>")
 keys = sorted(os.environ.keys())
 for key in keys:
     print(f"<tr><td><b>{key}</b></td><td>{os.environ[key]}</td></tr>")
 print("</table>")
 
-# --- 4. ECHO POST BODY (STDIN) ---
-# This verifies that your server correctly piped the request body to the child process
 print("<h2>POST Body (STDIN)</h2>")
 if os.environ.get('REQUEST_METHOD') == 'POST':
     try:
         content_length = os.environ.get('CONTENT_LENGTH')
         if content_length:
             length = int(content_length)
-            # Read exactly 'length' bytes
             body = sys.stdin.read(length) 
             print(f"<pre style='background:#eee; padding:10px;'>{body}</pre>")
         else:
